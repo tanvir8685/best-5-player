5094ed84fab08c0f1f5c924eae2a03b76fee7d68
tanvir <tanvirhridoy1011@gmail.com>
2022-08-22 20:20:11 +0600

    adding player name by clicking btn

--- js/best5.js
+++ js/best5.js
@@ -1,11 +1,31 @@
 const playerArray=[];
+function displayPlayer(player){
+    const addedPlayers=document.getElementById('added-players');
+    addedPlayers.innerHTML='';
+    for(let i=0;i<player.length;i++){
+
+        // console.log(playerArray[i]);
+        const playerName=playerArray[i];
+        console.log(playerName)
+        const li=document.createElement('li');
+        li.innerHTML=
+        `${playerName}`;
+        addedPlayers.appendChild(li)
+        
+    }
+    
+
+}
 function addToOl(element){
     // console.log(element.parentNode.children);
     // console.log(element.parentNode.children[0].innerText)
     const playerName=element.parentNode.children[0].innerText;
     playerArray.push(playerName);
+    
     // console.log(playerArray);
     // console.log(playerArray.length);
     document.getElementById('selected-player-number').innerText=playerArray.length;
+    displayPlayer(playerArray);
+    
     
 }
