aa5ba2db02951e72e5c006214c572b05b0739239
tanvir <tanvirhridoy1011@gmail.com>
2022-08-22 17:09:40 +0600

    identify btn value and top player number by array length

--- js/best5.js
+++ js/best5.js
@@ -1,1 +1,11 @@
-console.log('its connected')
+const playerArray=[];
+function addToOl(element){
+    // console.log(element.parentNode.children);
+    // console.log(element.parentNode.children[0].innerText)
+    const playerName=element.parentNode.children[0].innerText;
+    playerArray.push(playerName);
+    // console.log(playerArray);
+    // console.log(playerArray.length);
+    document.getElementById('selected-player-number').innerText=playerArray.length;
+    
+}
