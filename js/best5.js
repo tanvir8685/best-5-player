29940b4078bcb25299e074193f5a10eeb363bbd9
tanvir <tanvirhridoy1011@gmail.com>
2022-08-23 01:50:41 +0600

    player expenses

--- js/best5.js
+++ js/best5.js
@@ -1,31 +1,56 @@
-const playerArray=[];
-function displayPlayer(player){
-    const addedPlayers=document.getElementById('added-players');
-    addedPlayers.innerHTML='';
-    for(let i=0;i<player.length;i++){
+const playerArray = []
+
+function displayPlayer(player) {
+    const addedPlayers = document.getElementById('added-players');
+    addedPlayers.innerHTML = '';
+    for (let i = 0; i < player.length; i++) {
 
         // console.log(playerArray[i]);
-        const playerName=playerArray[i];
-        console.log(playerName)
-        const li=document.createElement('li');
-        li.innerHTML=
-        `${playerName}`;
+        const playerName = playerArray[i];
+        // console.log(playerName)
+        const li = document.createElement('li');
+        li.innerHTML =
+            `${playerName}`;
         addedPlayers.appendChild(li)
-        
+
     }
-    
+
 
 }
-function addToOl(element){
+function addToOl(element) {
     // console.log(element.parentNode.children);
     // console.log(element.parentNode.children[0].innerText)
-    const playerName=element.parentNode.children[0].innerText;
+    const playerName = element.parentNode.children[0].innerText;
     playerArray.push(playerName);
-    
+
     // console.log(playerArray);
     // console.log(playerArray.length);
-    document.getElementById('selected-player-number').innerText=playerArray.length;
+    document.getElementById('selected-player-number').innerText = playerArray.length;
+    let x = playerArray.length;
     displayPlayer(playerArray);
     
-    
+    const perPlayerBudgetCalculate=document.getElementById('per_player_budget_calulate')
+    perPlayerBudgetCalculate.addEventListener('click',function(){
+       const perPlayerBugetField=document.getElementById('per_player_budget');
+       
+       const perPlayerBugetValue=perPlayerBugetField.value;
+       
+       
+       const playerBudgetShowElement=document.getElementById('player-budget-show');
+       playerBudgetShowElement.innerText=perPlayerBugetValue*parseFloat(x);
+       
+       
+   
+    })
+
+    }
+
+
+
+// addToOl('selected-player-number');
+// console.log( document.getElementById('selected-player-number').innerHTML)
+
+
+function y(value)  {
+  ;
 }
